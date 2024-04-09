2cf964d6cf56466b7cb74194b84c1ba64d964f62
Chang Chen <baibaichen@gmail.com>
2024-04-09 11:07:41 +0800

    https://github.com/ClickHouse/ClickHouse/pull/61984 Make the accessibility of arrowColumnsToCHChunk private, and here it becomes public
    Fix rebase issue:
    - 20250507 https://github.com/ClickHouse/ClickHouse/pull/79777
    
    (cherry picked from commit 95f24b0e66f59e7ee424e8399f645bbe5f0ebe8b)

--- src/Processors/Formats/Impl/ArrowColumnToCHColumn.h
+++ src/Processors/Formats/Impl/ArrowColumnToCHColumn.h
@@ -58,7 +58,6 @@ public:
         UInt64 dictionary_size;
     };
 
-private:
     struct ArrowColumn
     {
         std::shared_ptr<arrow::ChunkedArray> column;
@@ -73,6 +72,7 @@ private:
         std::shared_ptr<const arrow::KeyValueMetadata> metadata,
         BlockMissingValues * block_missing_values);
 
+private:
     const Block & header;
     const std::string format_name;
 
