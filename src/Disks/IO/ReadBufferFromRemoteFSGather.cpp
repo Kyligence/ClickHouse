a79deabd5b32c4de46aa96ba01218ca7d897fbc1
LiuNeng <1398775315@qq.com>
2024-08-08 14:48:09 +0800

    Support merge MergeTree files (#498)
    (cherry picked from commit bf54eb6f5551ca083044c99bb674a8626835fe21)
    
    Fix rebase issue:
    - 20250713 https://github.com/ClickHouse/ClickHouse/pull/84003

--- src/Disks/IO/ReadBufferFromRemoteFSGather.cpp
+++ src/Disks/IO/ReadBufferFromRemoteFSGather.cpp
@@ -42,9 +42,14 @@ SeekableReadBufferPtr ReadBufferFromRemoteFSGather::createImplementationBuffer(c
     current_object = object;
     auto buf = read_buffer_creator(/* restricted_seek */true, object);
 
-    if (read_until_position > start_offset && read_until_position < start_offset + object.bytes_size)
-        buf->setReadUntilPosition(read_until_position - start_offset);
-
+    if (object.offset || (read_until_position > start_offset && read_until_position < start_offset + object.bytes_size))
+    {
+        if (read_until_position)
+            buf->setReadUntilPosition(object.offset + read_until_position - start_offset);
+        else
+            buf->setReadUntilPosition(object.offset + object.bytes_size);
+    }
+    
     return buf;
 }
 
@@ -70,7 +75,7 @@ void ReadBufferFromRemoteFSGather::initialize()
                 current_buf = createImplementationBuffer(object, start_offset);
             }
 
-            current_buf->seek(file_offset_of_buffer_end - start_offset, SEEK_SET);
+            current_buf->seek(object.offset + file_offset_of_buffer_end - start_offset, SEEK_SET);
             return;
         }
 
@@ -126,7 +131,7 @@ bool ReadBufferFromRemoteFSGather::readImpl()
         chassert(current_buf->available());
         chassert(
             blobs_to_read.size() != 1
-            || file_offset_of_buffer_end == current_buf->getFileOffsetOfBufferEnd(),
+            || file_offset_of_buffer_end + current_object.offset == current_buf->getFileOffsetOfBufferEnd(),
             fmt::format(
                 "offset: {}, buf offset: {}, available: {}, nextimpl offset: {}",
                 file_offset_of_buffer_end, current_buf->getFileOffsetOfBufferEnd(),
